60be78c1593736b49bb3e6b31c6cb83ec2250912
devesh0099 <deveshbhushan8@gmail.com>
2025-04-10 15:12:47 +0530

    Added DB

--- api/backend.mjs
+++ api/backend.mjs
@@ -1,9 +1,20 @@
 import express, { response } from "express";
 import dotenv from "dotenv";
+import pkg from "pg";
+const { Pool } = pkg;
 dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT_BACKEND || 3000;
+
+const pgPool = new Pool ({
+    user: 'appuser',
+    password: '1234',
+    host: 'localhost',
+    port: 5000,
+    database: 'appdb',
+    host: '../connection',
+  });
 
 app.listen(PORT, () => {
     console.log(`Running on Port: ${PORT}`);
@@ -11,4 +22,8 @@ app.listen(PORT, () => {
 
 app.get("/", (request,response) => {
     response.send(`Hello World! ${PORT}` );
+});
+
+app.get("/menu", (request,response) => {
+    // db se menu lena hai??
 });
